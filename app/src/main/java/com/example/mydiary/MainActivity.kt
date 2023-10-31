d695fedc1c234d5f4226b1ac76a96558fc619fb4
Arsenyi Kobelev <vilain140e@gmail.com>
2023-10-31 19:30:53 +0500

    dependencies

---
+++ app/src/main/java/com/example/mydiary/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.mydiary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mydiary.ui.theme.MyDiaryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var count by remember {
+                mutableStateOf(0)
+            }
+            MyDiaryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    IconButton(onClick = { count++ }) {
+                        Text(text = "$count")
+                    }
+                }
+            }
+        }
+    }
+}
+
